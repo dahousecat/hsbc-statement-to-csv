cb06aacf21fc510deec555792cbb09f2565242cc
Felix Eve <dahousecat@gmail.com>
2013-03-02 13:30:47 +0000

    initial commit

---
+++ create-csv.js
@@ -0,0 +1,61 @@
+var csv = '';
+var nl = '\n';
+var $table = $('table:not(".extPibTable")');
+var statement_date = $('.extContentHighlightPib:eq(1) .extPibRow:eq(0) .hsbcTextRight').html();
+var year = statement_date.substr(statement_date.length-4);
+
+// build header
+$('thead th', $table).each(function(){
+	if($('a', $(this)).length) {
+		csv = csv + '"' + $('strong', $(this)).html() + '",';
+	} else {
+		csv = csv + '"' + $(this).html() + '"';
+	}
+});
+
+csv = csv + nl;
+
+// get rest of data
+
+// loop rows
+$('tbody tr', $table).each(function(){
+	
+	// loop cells
+	var cell_count = 0;
+	$('td', $(this)).each(function(){
+		
+		if(cell_count==0) {
+			// this is the date
+			csv = csv + '"' + $('p', $(this)).html().trim() + ' ' + year + '",';
+		} else if(cell_count==5) {
+			// this is the balance
+			
+			var balance = $('p', $(this)).html().trim().replace('<b>', '').replace('</b>', '');
+			if($('p', $(this).next()).html().trim()=='D') {
+				balance = '-' + balance;
+			}
+			csv = csv + '"' + balance + '"';
+			
+		} else if(cell_count!=6) {
+			
+			if($('a', $(this)).length) {
+				csv = csv + '"' + $('a', $(this)).html().trim() + '",';
+			} else {
+				
+				if($('strong', $(this)).length) {
+					csv = csv + '"' + $('strong', $(this)).html().trim().replace('<b>', '').replace('</b>', '') + '",';
+				} else {
+					csv = csv + '"' + $('p', $(this)).html().trim().replace('&nbsp;', '').replace('<b>', '').replace('</b>', '') + '",';
+				}
+				
+			}
+		}
+		
+		cell_count++;
+	});
+	
+	csv = csv + nl;
+	
+});
+
+window.location = 'data:application/csv;charset=utf-8,' + encodeURIComponent(csv);
